41ee0ec9edfa8e5bf1b3281e4aa264676072295f
ilya osovets <iosovets@gmail.com>
2016-03-09 18:41:05 -0800

    First afterhours commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var nameArray = ["sean","jenny","kate", "uncle phil", "erik"];
+
+for(var i=0; i<nameArray.length; i++){
+  console.log("One of the names is " +nameArray[i]);
+
+}
+
+var iwhile=0;
+  while (iwhile < nameArray.length) {
+    console.log(nameArray[iwhile]);
+    iwhile++;
+}
+
+
